d2715ed7cdbd2e00889d5b5a999a3e2efa10b126
Michel Couillard <mcouillard@hortau.com>
2022-10-07 19:09:26 -0400

    Expose RNDateTimePicker

--- index.tsx
+++ index.tsx
@@ -2,5 +2,6 @@ import WheelPicker from './src/WheelPicker'
 import TimePicker from './src/TimePicker'
 import DatePicker from './src/DatePicker'
 import { Picker } from '@react-native-picker/picker'
+import RNDateTimePicker from '@react-native-community/datetimepicker';
 
-export { WheelPicker, TimePicker, DatePicker, Picker }
+export { WheelPicker, TimePicker, DatePicker, Picker, RNDateTimePicker }

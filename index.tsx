d2a7b1f15eb8b5e3c47eb0921b75ba008c447e6f
Michel Couillard <mcouillard@hortau.com>
2022-10-07 18:43:01 -0400

    Expose Picker to fix Invariant Violation: two views with RNCPicker

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import WheelPicker from './src/WheelPicker'
 import TimePicker from './src/TimePicker'
 import DatePicker from './src/DatePicker'
+import { Picker } from '@react-native-picker/picker'
 
-export { WheelPicker, TimePicker, DatePicker }
+export { WheelPicker, TimePicker, DatePicker, Picker }
